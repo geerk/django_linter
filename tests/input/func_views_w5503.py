e5c0ff5ca86503a1e981f582414ab60acd1908ba
Timofey Trukhanov <timofey.trukhanov@gmail.com>
2015-08-08 10:22:13 +0300

    add test for W5503

---
+++ tests/input/func_views_w5503.py
@@ -0,0 +1,15 @@
+"""Check for uneffecient query to get number of objects using len"""
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
+
+from .models import Product, Category
+
+
+def product_list_view(request):
+    """product_list_view"""
+    if request.is_authenticated():
+        ctx = {'products': Product.objects.all(),
+               'categories_count': len(Category.objects.all())}
+        return render(request, 'product_list.html', context=ctx)
+    else:
+        return HttpResponseForbidden()
