837fb00491dd15a953d3a85498dfd4e7cbc80bde
Timofey Trukhanov <timofey.trukhanov@gmail.com>
2015-08-08 10:18:19 +0300

    add test for W5502

---
+++ tests/input/func_views_w5502.py
@@ -0,0 +1,51 @@
+"""
+Check calling objects.get inside try-except
+"""
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from django.shortcuts import render
+from django.http import HttpResponseForbidden, HttpResponseNotFound
+from django.core.exceptions import MultipleObjectsReturned
+from django.views.generic import TemplateView
+
+from .models import Product, Category
+
+
+def product_list_view(request, cat_id):
+    """product_list_view"""
+    if request.is_authenticated():
+        ctx = {'products': Product.objects.all(),
+               'cat_id': cat_id}
+        return render(request, 'product_list.html', context=ctx)
+    else:
+        return HttpResponseForbidden()
+
+
+def product_detail_view(request, slug):
+    """product_detail_view"""
+    if request.is_authenticated:
+        try:
+            product = Product.objects.get(slug=slug)
+        except MultipleObjectsReturned:
+            return HttpResponseNotFound()
+        return render(request, 'product_detail.html', context={'product': product})
+    else:
+        return HttpResponseForbidden()
+
+
+def category_detail_view(request, **kwargs):
+    """category_detail_view"""
+    try:
+        category = Category.objects.get(pk=kwargs['pk'])
+    except Category.DoesNotExist:
+        return HttpResponseNotFound()
+    return render(request, 'category_detail.html', context={'category': category})
+
+
+class IndexView(TemplateView):
+    """IndexView"""
+
+    def get_context_data(self, **kwargs):
+        ctx = super(IndexView, self).get_context_data(**kwargs)
+        ctx['cat'] = Category.objects.get(pk=kwargs['cat_id'])
+        return ctx
