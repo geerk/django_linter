56516eb0a3af8bcc881dea5fb9dc718a2e310dfb
Timofey Trukhanov <timofey.trukhanov@gmail.com>
2015-08-16 17:12:57 +0300

    rename test files, bump version to 0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django_linter',
-    version='0.5.dev3',
+    version='0.5',
     packages=('django_linter', 'django_linter.checkers',
               'django_linter.suppressers', 'django_linter.transformers'),
     description='Linter for django projects',
