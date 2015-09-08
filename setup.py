d4a4846c38714f6115b4237031c3d01dbcbf968b
Timofey Trukhanov <timofey.trukhanov@gmail.com>
2015-09-08 19:18:37 +0300

    bump version to 0.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_linter',
-    version='0.6.dev3',
+    version='0.6',
     packages=find_packages(exclude=['tests*']),
     description='Linter for django projects',
     long_description=open('README.rst').read(),
